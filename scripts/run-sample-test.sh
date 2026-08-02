a99d3db4be40cf65334803cf155442a75f0213bf
Jihyeon Choung <jihyeon.choung@gmail.com>
2026-08-02 14:23:37 +0200

    test: add container sample test runner

---
+++ scripts/run-sample-test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -eu
+
+cd /workspace
+
+defaults_file="$(mktemp)"
+trap 'rm -f "$defaults_file"' EXIT
+chmod 600 "$defaults_file"
+
+printf '%s\n' \
+    '[client]' \
+    'host=127.0.0.1' \
+    'user=root' \
+    "password=$MYSQL_ROOT_PASSWORD" \
+    'local-infile=1' \
+    > "$defaults_file"
+
+mysql \
+    --defaults-extra-file="$defaults_file" \
+    < tests/run_sample_idempotency.sql
